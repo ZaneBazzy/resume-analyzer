ab63df9507ac5d21f4f52c4460be94ca6000eb92
Zane Bazzy <Zaneb2017@gmail.com>
2026-04-06 21:28:17 -0400

    Added score calc

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ for skill in job_skills:
     else:
         missing_skills.append(skill)
 
+match_score = (len(matched_skills) / len(job_skills)) * 100
+
 print("Matched Skills:", matched_skills)
 print("Missing Skills:", missing_skills)
-
+print("Match Score:", round(match_score, 2), "%")
