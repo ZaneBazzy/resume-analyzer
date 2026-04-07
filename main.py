3452c0e52745172b95adefe30abe8aee4a37f690
Zane Bazzy <Zaneb2017@gmail.com>
2026-04-06 21:08:58 -0400

    Added basic skill matching logic

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Python Resume Analyzer")
+resume_text = """
+Python
+Java
+SQL
+Git
+"""
+
+job_description = """
+Python
+SQL
+AWS
+Machine Learning
+Git
+"""
+
+
+resume_skills = resume_text.lower().split()
+job_skills = job_description.lower().split()
+
+matched_skills = []
+missing_skills = []
+
+for skill in job_skills:
+    if skill in resume_skills:
+        matched_skills.append(skill)
+    else:
+        missing_skills.append(skill)
+
+print("Matched Skills:", matched_skills)
+print("Missing Skills:", missing_skills)
+
