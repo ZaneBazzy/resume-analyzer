8dbb159308a88c38746108d61085d618c20c03d6
Zane Bazzy <Zaneb2017@gmail.com>
2026-04-09 14:52:53 -0400

    Refactored code into analyze_resume function

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+
+def analyze_resume(resume_text, job_description):
+    resume_skills = resume_text.lower().split()
+    job_skills = job_description.lower().split()
+
+    matched_skills = []
+    missing_skills = []
+
+    for skill in job_skills:
+        if skill in resume_skills:
+            matched_skills.append(skill)
+        else:
+            missing_skills.append(skill)
+
+    match_score = (len(matched_skills) / len(job_skills)) * 100
+
+    return matched_skills, missing_skills, match_score
+
 resume_text = """
 Python
 Java
@@ -13,21 +31,10 @@ Machine Learning
 Git
 """
 
+matched, missing, score = analyze_resume(resume_text, job_description)
 
-resume_skills = resume_text.lower().split()
-job_skills = job_description.lower().split()
 
-matched_skills = []
-missing_skills = []
+print("Matched Skills:", matched)
+print("Missing Skills:", missing)
+print("Match Score:", round(score, 2), "%")
 
-for skill in job_skills:
-    if skill in resume_skills:
-        matched_skills.append(skill)
-    else:
-        missing_skills.append(skill)
-
-match_score = (len(matched_skills) / len(job_skills)) * 100
-
-print("Matched Skills:", matched_skills)
-print("Missing Skills:", missing_skills)
-print("Match Score:", round(match_score, 2), "%")
